2ea9ab9d07de9eed8029fe6b5290d9df3fb6cd3f
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-15 19:55:57 +0800

    Added link to mongo database and routes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
+const apiRoutes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,13 +15,21 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-
+app.use("/api", apiRoutes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+mongoose.connect(
+  "mongodb://localhost/googlebooks",
+  { useNewUrlParser: true },
+  function (error) {
+    if (error) throw error;
+
+    app.listen(PORT, () => {
+      console.log(`🌎 ==> API server now on port ${PORT}!`);
+    });
+  }
+);
