b51af9ae151c494d2eaee3c767d25c1584f97d66
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-15 21:53:41 +0800

    Created api.js to fetch books from google books API

---
+++ client/src/utils/api.js
@@ -0,0 +1,11 @@
+export default {
+    async searchBooks(title){
+        const url = `https://www.googleapis.com/books/v1/volumes?q=${title}`;
+        const response = await fetch(url);
+        return response.json();
+    }
+
+    async saveBook(book){
+        const response = await fetch
+    }
+}
