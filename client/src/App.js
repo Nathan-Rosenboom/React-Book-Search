176522142acd879c9d45f4ff93d098dfae61d088
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-15 21:51:03 +0800

    Imported search.js page into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/search";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path={['/', '/search']}>
+            <Search />
+          </Route>
+        </Switch>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
 
